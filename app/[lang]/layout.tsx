c04f3561d9d0978c63c956471d24494eab211c75
Dicki Prasetya <dickiprasetya176@gmail.com>
2023-07-09 19:23:40 +0700

    feat: make update

--- app/[lang]/layout.tsx
+++ app/[lang]/layout.tsx
@@ -56,6 +56,7 @@ export const generateMetadata = async ({
       },
     },
     /* Verification for Google Search Console */
+
     verification: {
       google: "8xnhMuMJZnHzVBeWXO6w0Tt9AuOvkc8XpKP3DYOjNGk",
     },
